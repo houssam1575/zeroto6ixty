224c1ccb76a15740ad8a703523d803a9ded60cb7
Houssam Farhat <houssamfarhat@Houssams-MacBook-Pro.local>
2019-02-20 00:54:42 +1100

    Hello world

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bp = require('body-parser');
+const ejs = require('ejs');
+
+
+app.set('view engine', 'ejs');
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+app.get("/", function(req, res) {
+  res.render("index");
+  console.log("redirected to /index.ejs");
+});
