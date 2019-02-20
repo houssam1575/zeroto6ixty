7f1304f29e4941f037d5ff72ca6bd584038178af
Houssam Farhat <houssamfarhat@Houssams-MacBook-Pro.local>
2019-02-20 21:25:19 +1100

    Added new logo

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs');
 app.listen(process.env.PORT || 3000, function() {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+app.use('/assets/', express.static("./assets/"));
 
 app.get("/", function(req, res) {
   res.render("index");
