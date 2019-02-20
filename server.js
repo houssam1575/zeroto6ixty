2a228a031e387be69ded336df90851b8ea9290c4
Houssam Farhat <houssamfarhat@Houssams-MacBook-Pro.local>
2019-02-20 22:04:51 +1100

    Image width/height CSS fix #2

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.listen(process.env.PORT || 3000, function() {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 app.use('/assets/', express.static("./assets/"));
+app.use('/css/', express.static("./css/"));
 
 app.get("/", function(req, res) {
   res.render("index");
