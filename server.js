03d16b20073111b7358831628e5bfce949870c82
Houssam Farhat <houssamfarhat@Houssams-MacBook-Pro.local>
2019-02-22 23:30:05 +1100

    Added demo gallery page

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const express = require('express');
 const app = express();
 const bp = require('body-parser');
 const ejs = require('ejs');
+var galleryTitles = [
+  "Japan",
+  "Cars"
+];
 
 
 app.set('view engine', 'ejs');
+app.use(bp.urlencoded({
+  extended: true
+}));
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
@@ -20,6 +27,8 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  res.render("gallery");
+  res.render("gallery", {
+    newListItems: galleryTitles
+  });
   console.log("redirected to /gallery.ejs");
 });
