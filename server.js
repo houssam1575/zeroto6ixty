d9ad2075a487b337ecb0dfbe140ecb7993655942
Houssam Farhat <houssamfarhat@Houssams-MacBook-Pro.local>
2019-02-23 22:44:23 +1100

    Restored to older version

--- server.js
+++ server.js
@@ -13,30 +13,6 @@ app.use(bp.urlencoded({
   extended: true
 }));
 
-const privateKey = fs.readFileSync('ssl/letsencrypt/live/www.zeroto6ixty.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('ssl/letsencrypt/live/www.zeroto6ixty.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('ssl/letsencrypt/live/www.zeroto6ixty.com/chain.pem', 'utf8');
-
-
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
-
-// Starting both http & https servers
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(process.env.PORT || 3000, () => {
-	console.log('HTTP Server running on port 3000');
-});
-
-httpsServer.listen(process.env.PORT || 443, () => {
-	console.log('HTTPS Server running on port 443');
-});
-app.use('/assets/', express.static("./assets/"));
-app.use('/css/', express.static("./css/"));
 
 var obj;
 fs.readFile('json/ALBUMS.json', 'utf8', function(err, data) {
@@ -52,6 +28,11 @@ app.use(bp.urlencoded({
   extended: true
 }));
 
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+app.use('/assets/', express.static("./assets/"));
+app.use('/css/', express.static("./css/"));
 app.get("/", function(req, res) {
   res.render("welcome");
   console.log("redirected to /index.ejs");
