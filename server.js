75f44988259e88fd779c82658d2417efff9fe2e8
Houssam Farhat <houssamfarhat@Houssams-MacBook-Pro.local>
2019-02-23 21:58:24 +1100

    SSL added

--- server.js
+++ server.js
@@ -4,15 +4,29 @@ const express = require('express');
 const app = express();
 const bp = require('body-parser');
 const ejs = require('ejs');
-
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
 var galleryTitles = [];
 app.use(bp.urlencoded({
   extended: true
 }));
 
+const privateKey = fs.readFileSync('ssl/letsencrypt/live/www.zeroto6ixty.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('ssl/letsencrypt/live/www.zeroto6ixty.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('ssl/letsencrypt/live/www.zeroto6ixty.com/chain.pem', 'utf8');
 
-var fs = require('fs');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 var obj;
 fs.readFile('json/ALBUMS.json', 'utf8', function(err, data) {
   if (err) throw err;
@@ -27,12 +41,15 @@ app.use(bp.urlencoded({
   extended: true
 }));
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
 });
 app.use('/assets/', express.static("./assets/"));
 app.use('/css/', express.static("./css/"));
-app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 app.get("/", function(req, res) {
   res.render("welcome");
   console.log("redirected to /index.ejs");
