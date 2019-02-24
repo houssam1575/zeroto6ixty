ed9ccb198a5619b707e7cd92305c3a72a0f8f560
Houssam Farhat <houssamfarhat@Houssams-MacBook-Pro.local>
2019-02-24 22:15:03 +1100

    Added pages, dynamic routing depending on album name

--- server.js
+++ server.js
@@ -7,22 +7,21 @@ const ejs = require('ejs');
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-
 var galleryTitles = [];
+
 app.use(bp.urlencoded({
   extended: true
 }));
 
-
-var obj;
 fs.readFile('json/ALBUMS.json', 'utf8', function(err, data) {
   if (err) throw err;
-  var obj = JSON.parse(data);
+  obj = JSON.parse(data);
   for (var x in obj.album) {
     galleryTitles.push(x);
     console.log("Adding: " + x);
   }
 });
+
 app.set('view engine', 'ejs');
 app.use(bp.urlencoded({
   extended: true
@@ -31,6 +30,7 @@ app.use(bp.urlencoded({
 app.listen(process.env.PORT || 3000, function() {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
 app.use('/assets/', express.static("./assets/"));
 app.use('/css/', express.static("./css/"));
 app.get("/", function(req, res) {
@@ -43,4 +43,49 @@ app.post("/gallery", function(req, res) {
     newListItems: galleryTitles,
   });
   console.log("redirected to /gallery.ejs");
+});
+app.get('/:user/:token', function(req, res) {
+  albumName = req.params.token;
+  console.log("User is requesting: " + albumName);
+  console.log("Finding token...");
+  for (var i = 0; i < galleryTitles.length; i++) {
+    if (galleryTitles[i] === albumName) {
+      console.log("found token: " + albumName);
+      console.log("Album data: " + obj.album[albumName]);
+      populateAlbumFile(albumName);
+      res.redirect("/album");
+      break;
+    }
+    else {
+
+    }
+  }
+
+  function populateAlbumFile(token) {
+    var imageLinks = [];
+    var count = 0;
+    if (obj.album[token] === undefined) {
+      console.log("data is undefined, album won't load");
+      console.log(obj.album[token]);
+    } else {
+      console.log("Populating album for: " + token);
+      for (var x in obj.album[token]) {
+        imageLinks.push(x);
+        count++;
+      }
+      if (count === 0) {
+        console.log("Error: could not populate album!");
+      } else {
+        console.log("Finished populating");
+        console.log(imageLinks);
+        app.get('/album', function(req,res) {
+          res.render("album", {
+            albumTitle: albumName,
+            imageLinks: imageLinks
+          });
+          console.log("redirected to " + albumName + " album");
+        });
+      }
+    }
+  }
 });
