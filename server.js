165556f4ec0cf16956ecdd1f1a5e741a42113ba1
Houssam Farhat <houssamfarhat@Houssams-MacBook-Pro.local>
2019-02-27 16:06:36 +1100

    Added Japan pictures

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const http = require('http');
 const https = require('https');
 var galleryTitles = [];
 
+
 app.use(bp.urlencoded({
   extended: true
 }));
 //This reads ALBUMS.json, if you want to add more pictures/albums, use ALBUMS.json to add
-
 fs.readFile('json/ALBUMS.json', 'utf8', function(err, data) {
   if (err) throw err;
   obj = JSON.parse(data);
@@ -28,10 +28,12 @@ app.set('view engine', 'ejs');
 app.use(bp.urlencoded({
   extended: true
 }));
+
 //SERVER CONFIGURATION
 app.listen(process.env.PORT || 3000, function() {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
 //These are the directories to deliver static files
 app.use('/assets/', express.static("./assets/"));
 app.use('/css/', express.static("./css/"));
@@ -42,6 +44,7 @@ app.get("/", function(req, res) {
   res.render("welcome");
   console.log("redirected to /index.ejs");
 });
+
 app.post("/gallery", function(req, res) {
   res.render("gallery", {
     newListItems: galleryTitles,
@@ -54,6 +57,7 @@ app.get('/:user/:token', function(req, res) {
   albumName = req.params.token;
   console.log("User is requesting: " + albumName);
   console.log("Finding token...");
+
   for (var i = 0; i < galleryTitles.length; i++) {
     if (galleryTitles[i] === albumName) {
       console.log("found token: " + albumName);
@@ -68,38 +72,68 @@ app.get('/:user/:token', function(req, res) {
 
   function populateAlbumFile(token) {
     var count = 0;
+
     if (obj.album[token] === undefined || obj.album[token].length === 0) {
       console.log("data is undefined, album won't load");
     } else {
       var imageLinks = [];
       var albumLinks = [];
+      var galleryTitlesDescription = [];
+
       console.log(imageLinks);
       console.log("Populating album for: " + token);
       //This pushes the names of the albums, etc japan
+
       for (var x in obj.album[token]) {
         imageLinks.push(x);
       }
+
+      console.log(galleryTitlesDescription);
       var iterateAmount = 0;
+
       //this pushes the links that are stored into the album
-      for(var i = 0; i < obj.album[token][token + "0"].length -1; i++) {
+      for (var i = 0; i < obj.album[token][token + "0"].length - 1; i++) {
         albumLinks.push(obj.album[token][token + iterateAmount]);
         iterateAmount++;
-        console.log(iterateAmount);
       }
-        console.log(albumLinks);
-        newImageArray = imageLinks;
-        newAlbumLinks = albumLinks;
-        console.log("Population completed");
-        app.get('/album', function(req, res) {
-          res.render("album", {
-            albumTitle: albumName,
-            imageLinks: newImageArray,
-            albumLinks: newAlbumLinks
-          });
-          console.log("redirected to " + albumName + " album");
-          console.log("Array passed into " + albumName + ": " + newImageArray);
-          console.log("Array links passed into " + albumName + ": " + newAlbumLinks);
+
+      //this pushes the description of the images provided by the user into the array called galleryTitlesDescription
+      var counter = 0;
+
+      console.log("Trying to populate descriptions...");
+      console.log("Array length" + obj.description["description" + token][token + counter].length);
+      galleryTitlesDescription = [];
+
+      for (var j = 0; i < obj.description["description" + token][token + "0"].length; j++); {
+        galleryTitlesDescription.push(obj.description["description" + token][token + counter]);
+        console.log("Adding to galleryTitlesDescription: " + obj.description["description" + token][token + counter]);
+        counter++;
+      }
+
+      //This posts the data to album.EJS
+
+      console.log("Finished adding description to " + token + " image description, the data added: " + galleryTitlesDescription);
+      console.log(albumLinks);
+
+      newImageArray = imageLinks;
+      newAlbumLinks = albumLinks;
+      newGalleryTitlesDescription = galleryTitlesDescription;
+
+      console.log("Population completed");
+
+      app.get('/album', function(req, res) {
+        res.render("album", {
+          albumTitle: albumName,
+          imageLinks: newImageArray,
+          albumLinks: newAlbumLinks,
+          description: newGalleryTitlesDescription
         });
+
+        console.log("redirected to " + albumName + " album");
+        console.log("Array passed into " + albumName + ": " + newImageArray);
+        console.log("Array links passed into " + albumName + ": " + newAlbumLinks);
+        console.log("Descriptions added to " + albumName + ": " + newGalleryTitlesDescription);
+      });
     }
   }
 });
